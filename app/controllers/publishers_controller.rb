52bd2b0bebecf31b6f877eaaf92ccce425273125
Edwinna <nalcarya1881@gmail.com>
2018-08-02 10:21:10 +0300

    books

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -9,6 +9,21 @@ class PublishersController < ApplicationController
   def show; end
 
   def shops
+    # binding.pry
+    hashh = Shop.all.joins(shop_books: :book).where("books.publisher_id = ?", @publisher.id).reduce([]) do |acc, shop|
+      acc << {
+        id: shop.id,
+        name: shop.name,
+        # books_sold_count: shop.shop_books.sold_count,
+        books_in_stock: shop.shop_books.map { |shop_book|
+          { id: shop_book.book_id,
+            title: shop_book.book.title,
+            copies_in_stock: shop_book.count,
+            publisher_id: shop_book.book.publisher_id
+          }
+        }
+      }
+    end
     binding.pry
     render status: 200
   end
